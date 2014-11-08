cc1eea558281a66bb76c76146835427ddda56bbd
rbarzyczak <roman.barzyczak@brainly.com>
2014-11-08 17:33:59 +0100

    now you can configure more classes with own names

--- BestChoiceABTest.java
+++ BestChoiceABTest.java
@@ -4,43 +4,4 @@ package com.zdobywacz.ab;
  * Created by yoman on 08.11.14.
  */
 public class BestChoiceABTest {
-    public void A() {
-
-    }
-
-    public void B() {
-
-    }
-
-    public void C() {
-
-    }
-
-    public void D() {
-
-    }
-
-    public void E() {
-
-    }
-
-    public void F() {
-
-    }
-
-    public void G() {
-
-    }
-
-    public void H() {
-
-    }
-
-    public void I() {
-
-    }
-
-    public void J() {
-
-    }
 }
